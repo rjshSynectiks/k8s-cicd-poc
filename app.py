ccdabae20b1159eb9c910048cba916ee487e2cf7
Rajesh Borugadda <rajesh.borugadda@synectiks.com>
2026-02-12 15:56:14 +0530

    Initial commit - k8s cicd poc

---
+++ app.py
@@ -0,0 +1,10 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Kubernetes CI/CD POC "
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
